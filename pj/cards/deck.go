d89a15004c20dd33cee8801d9bdc421a1670678e
Takahiko Okada <takahikookada@gmail.com>
2018-04-11 22:19:19 +0200

    first testing implemented

--- pj/cards/deck.go
+++ pj/cards/deck.go
@@ -5,6 +5,8 @@ import (
   "strings"
   "io/ioutil"
   "os"
+  "math/rand"
+  "time"
 )
 
 // Create a new type of 'deck', a slice of strings
@@ -60,3 +62,14 @@ func newDeckFromFile(filename string) deck {
   s := strings.Split(string(bs), ",") //Ace of Spades, Two of Spades, ...
   return deck(s)
 }
+
+func (d deck) shuffle() {
+  source := rand.NewSource(time.Now().UnixNano())
+  r := rand.New(source)
+
+  for i := range d {
+    newPosition := r.Intn(len(d) - 1)
+    d[i], d[newPosition] = d[newPosition], d[i]
+  }
+}
+
--- pj/cards/main.go
+++ pj/cards/main.go
@@ -2,5 +2,7 @@ package main
 
 func main() {
   cards := newDeckFromFile("my_cards")
+  cards.shuffle()
   cards.print()
+
 }
